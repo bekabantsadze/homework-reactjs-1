2ac8793731170156cd8e54d1dbdaf0f7f421bd87
Beka Bantasdze <thelataito@gmail.com>
2022-12-26 20:33:54 +0400

    3 & 4

---
+++ react.js
@@ -0,0 +1,34 @@
+// დავალაევა 1
+
+// const array = [ 1, 2, 3, 4, 5, -2, 23, -1, -13, 10, -52 ],
+//     positive = array.filter(function (a) { return a >= 0; }),
+//     sum = positive.reduce(function (a, b) { return a + b; });
+
+// console.log(sum);
+
+// დავალება 2
+
+// const arr = ["nissan",  "masserati","mercedes", "mercedes",  "toyota",  "mercedes", "masserati",];
+// const count = {};
+
+// arr.forEach(element => {
+//   count[element] = (count[element] || 0) + 1;
+// });
+
+// console.log(count);
+
+// test
+
+// let sayMyName = new Promise(function (resolve, reject) {
+
+// 	setTimeout(function () {
+// 		resolve('Hi, Beka!');
+// 	}, 3000);
+
+// });
+// sayMyName.then(function (msg) {
+// 	console.log(msg);
+// });
+
+// დავალება 4 
+    
